0b56593f7e2f75565ee2fdac9e9c0a4a926cb563
Dhruv Aggarwal <dhruvaggarwal043@gmail.com>
2018-05-07 01:56:07 +0530

    Add test cases for regex route matching

--- tests/db_mode/conftest.py
+++ tests/db_mode/conftest.py
@@ -287,3 +287,233 @@ def fixture_failure(request):
     ]
     request.addfinalizer(tear_down)
     return data_to_send
+
+
+def db_data_setup_regex():
+    # Add Roles
+    base_role = Role(name='base')
+    admin_role = Role(name='admin_regex')
+    db.session.add(base_role)
+    db.session.add(admin_role)
+    db.session.commit()
+
+    # Add Users
+    base_user = User(name='base')
+    admin_user = User(name='admin_regex')
+    db.session.add(base_user)
+    db.session.add(admin_user)
+    db.session.commit()
+
+    # Attach role to users
+    db.session.add(UserRoleMap(role=base_role, user=base_user))
+    db.session.add(UserRoleMap(role=admin_role, user=admin_user))
+    db.session.commit()
+
+    # Add Routes
+
+    # Route for admin
+    all_get_route = Route(rule='.+', method='GET')
+    all_post_route = Route(rule='.+', method='POST')
+
+    # Route for get of all covered route with ids
+    int_route = Route(rule='/covered_route/\d+', method='GET')
+    # Route for posting on covered_route, id: 1
+    specific_id_route = Route(rule='/covered_route/1', method='POST')
+
+    # base user can also do a get of the covered route
+    exact_route = Route(rule='/covered_route', method='GET')
+    db.session.add(all_get_route)
+    db.session.add(all_post_route)
+    db.session.add(int_route)
+    db.session.add(specific_id_route)
+    db.session.add(exact_route)
+    db.session.commit()
+
+    # Add Role Route Map
+    # Admin is allowed to get, post all endpoints
+    db.session.add(RoleRouteMap(role=admin_role, route=all_post_route))
+    db.session.add(RoleRouteMap(role=admin_role, route=all_get_route))
+    # base user is allowed to get any covered route id but post only on id 1
+    db.session.add(RoleRouteMap(role=base_role, route=int_route))
+    db.session.add(RoleRouteMap(role=base_role, route=specific_id_route))
+    # base user can also do a get of the covered route
+    db.session.add(RoleRouteMap(role=base_role, route=exact_route))
+    db.session.commit()
+    db.session.refresh(base_user)
+    db.session.refresh(admin_user)
+    rrbac.init_app(app)
+    return [base_user, admin_user]
+
+
+@pytest.fixture(scope='function')
+def fixture_regex_success(request):
+    """
+    Test Cases:
+    1. GET covered_route for admin
+    2. POST covered_route for admin
+    3. GET covered_route/1 for admin
+    4. POST covered_route/1 for admin
+    5. GET uncovered_route for admin
+    6. GET covered_route/1 for base user
+    7. GET covered_route/2 for base user
+    8. POST covered_route/1 for base user
+    9. GET covered_route for base user
+    """
+
+    db.create_all()
+    base_user, admin_user = db_data_setup_regex()
+
+    data_to_send = [
+        {
+            'input': {
+                'method': 'GET',
+                'url_rule': '/covered_route',
+                'user': admin_user,
+                'function': 'covered_route'
+            },
+            'output': {
+                'status_code': 200
+            }
+        },
+        {
+            'input': {
+                'method': 'POST',
+                'url_rule': '/covered_route',
+                'user': admin_user,
+                'function': 'covered_route'
+            },
+            'output': {
+                'status_code': 200
+            }
+        },
+        {
+            'input': {
+                'method': 'GET',
+                'url_rule': '/covered_route/1',
+                'user': admin_user,
+                'function': 'number_covered_route'
+            },
+            'output': {
+                'status_code': 200
+            }
+        },
+        {
+            'input': {
+                'method': 'POST',
+                'url_rule': '/covered_route/1',
+                'user': admin_user,
+                'function': 'number_covered_route'
+            },
+            'output': {
+                'status_code': 200
+            }
+        },
+        {
+            'input': {
+                'method': 'GET',
+                'url_rule': '/uncovered_route',
+                'user': admin_user,
+                'function': 'uncovered_route'
+            },
+            'output': {
+                'status_code': 200
+            }
+        },
+        {
+            'input': {
+                'method': 'GET',
+                'url_rule': '/covered_route/1',
+                'user': base_user,
+                'function': 'number_covered_route'
+            },
+            'output': {
+                'status_code': 200
+            }
+        },
+        {
+            'input': {
+                'method': 'GET',
+                'url_rule': '/covered_route/2',
+                'user': base_user,
+                'function': 'number_covered_route'
+            },
+            'output': {
+                'status_code': 200
+            }
+        },
+        {
+            'input': {
+                'method': 'POST',
+                'url_rule': '/covered_route/1',
+                'user': base_user,
+                'function': 'number_covered_route'
+            },
+            'output': {
+                'status_code': 200
+            }
+        },
+        {
+            'input': {
+                'method': 'GET',
+                'url_rule': '/covered_route',
+                'user': base_user,
+                'function': 'covered_route'
+            },
+            'output': {
+                'status_code': 200
+            }
+        }
+    ]
+    request.addfinalizer(tear_down)
+    return data_to_send
+
+
+@pytest.fixture(scope='function')
+def fixture_regex_failure(request):
+    """
+    Test Cases:
+    1. POST covered_route/2 for base user
+    2. POST covered_route for base user
+    3. GET uncovered_route for base user
+    """
+
+    db.create_all()
+    base_user, admin_user = db_data_setup_regex()
+
+    data_to_send = [
+        {
+            'input': {
+                'method': 'POST',
+                'url_rule': '/covered_route/2',
+                'user': base_user,
+                'function': 'number_covered_route'
+            },
+            'output': {
+                'status_code': 403
+            }
+        },
+        {
+            'input': {
+                'method': 'POST',
+                'url_rule': '/covered_route',
+                'user': base_user,
+                'function': 'covered_route'
+            },
+            'output': {
+                'status_code': 403
+            }
+        },
+        {
+            'input': {
+                'method': 'GET',
+                'url_rule': '/uncovered_route',
+                'user': base_user,
+                'function': 'uncovered_route'
+            },
+            'output': {
+                'status_code': 403
+            }
+        }
+    ]
+    request.addfinalizer(tear_down)
+    return data_to_send
--- tests/db_mode/test_db_mode.py
+++ tests/db_mode/test_db_mode.py
@@ -1,5 +1,5 @@
 import pytest
-from . import app, uncovered_route, covered_route
+from . import app, uncovered_route, covered_route, number_covered_route
 from werkzeug.exceptions import Forbidden
 
 
@@ -20,6 +20,38 @@ class TestRRBAC():
     @pytest.mark.usefixtures("fixture_failure")
     def test_failure(self, fixture_failure):
         for index, data in enumerate(fixture_failure):
+            print '\nScenario {} Started'.format(index + 1)
+            with app.test_request_context(
+                data['input']['url_rule'],
+                method=data['input']['method']
+            ) as request_ctx:
+                if data['input']['user']:
+                    request_ctx.user = data['input']['user']
+                try:
+                    result = 0
+                    eval(data['input']['function'])()
+                except Forbidden:
+                    result = 1
+                finally:
+                    assert result
+                    print '\nScenario {} Passed'.format(index + 1)
+
+    @pytest.mark.usefixtures("fixture_regex_success")
+    def test_regex_success(self, fixture_regex_success):
+        for index, data in enumerate(fixture_regex_success):
+            print '\nScenario {} Started'.format(index + 1)
+            with app.test_request_context(
+                data['input']['url_rule'], method=data['input']['method']
+            ) as request_ctx:
+                if data['input']['user']:
+                    request_ctx.user = data['input']['user']
+                output = eval(data['input']['function'])()
+                assert output.status_code == data['output']['status_code']
+                print '\nScenario {} Passed'.format(index + 1)
+
+    @pytest.mark.usefixtures("fixture_regex_failure")
+    def test_regex_failure(self, fixture_regex_failure):
+        for index, data in enumerate(fixture_regex_failure):
             print '\nScenario {} Started'.format(index + 1)
             with app.test_request_context(
                 data['input']['url_rule'],
