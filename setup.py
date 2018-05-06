4c1188310ec1712607a39b45968fc255bf46e0dd
Dhruv Aggarwal <dhruvaggarwal043@gmail.com>
2018-05-06 23:07:21 +0530

    Setup for uploading to pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import dirname, realpath, join
 
 CURRENT_DIR = dirname(realpath(__file__))
 
-with open(join(CURRENT_DIR, "README.rst")) as long_description_file:
+with open(join(CURRENT_DIR, "README.md")) as long_description_file:
     long_description = long_description_file.read()
 
 setup(
@@ -14,20 +14,30 @@ setup(
     author_email="dhruvaggarwal043@gmail.com",
     description="Role Route Based Access Control support for Flask",
     long_description=long_description,
+    license='MIT',
     zip_safe=False,
-    packages=find_packages(exclude=["docs"]),
+    packages=find_packages(exclude=["docs", "tests*", "source", "build"]),
     include_package_data=True,
     platforms="any",
     install_requires=["Flask>=0.10"],
+    keywords='flask access control acl rbac',
+    python_requires='~=2.6',
     classifiers=[
         "Framework :: Flask",
         "Environment :: Web Environment",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python",
         "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-    ]
+        'Topic :: Software Development :: Build Tools',
+    ],
+    project_urls={
+        'Documentation':
+        'https://github.com/dhruv-aggarwal/flask-rrbac/blob/master/README.md',
+        'Source': 'https://github.com/dhruv-aggarwal/flask-rrbac',
+        'Tracker': 'https://github.com/dhruv-aggarwal/flask-rrbac/issues',
+    },
 )
